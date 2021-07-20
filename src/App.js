f5c8b81a8d233a749ada877f643fe8f5dc3571e2
Anthony Chapman <tony@sonderagency.com>
2021-07-20 10:43:09 -0600

    Add Pricing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Switch, BrowserRouter, Link, Route } from 'react-router-dom'
-import { Home, About, Contact } from './Pages'
+import { Home, About, Contact, Pricing } from './Pages'
 
 function App() {
   return (
@@ -10,11 +10,13 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/pricing">Pricing</Link>
       </header>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/pricing" component={Pricing} />
       </Switch>
       </BrowserRouter>
     </div>
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { Home } from './Home'
 export { About } from './About'
 export { Contact } from './Contact'
+export { Pricing } from './Pricing'
